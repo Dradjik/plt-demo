d34586b7ab4b647213db0f64650d8581cf962df0
dradjik <dradjik>
2026-03-02 13:44:37 +0100

    fix: remove ip mentions

--- modules/plt-demo/outputs.tf
+++ modules/plt-demo/outputs.tf
@@ -7,14 +7,3 @@ output "bucket_endpoint" {
   description = "Endpoint of the created bucket"
   value       = scaleway_object_bucket.test_bucket.endpoint
 }
-
-output "instance_ip_id" {
-  description = "ID of the created instance IP"
-  value       = scaleway_instance_ip.test_ip.id
-}
-
-output "instance_ip_address" {
-  description = "IP address of the created instance IP"
-  value       = scaleway_instance_ip.test_ip.address
-}
-
