870a651f83c38faf8cbadeca71af32ee5d24e80f
dradjik <dradjik>
2026-03-02 14:02:07 +0100

    fix: remove ref ip in main outpus

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,3 @@ output "bucket_endpoint" {
   value       = module.plt_demo.bucket_endpoint
 }
 
-output "instance_ip_id" {
-  description = "ID of the created instance IP"
-  value       = module.plt_demo.instance_ip_id
-}
-
-output "instance_ip_address" {
-  description = "IP address of the created instance IP"
-  value       = module.plt_demo.instance_ip_address
-}
-
